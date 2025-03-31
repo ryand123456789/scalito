55e135fa1e1f78a4b9a38ea39b53661d3920dc70
ryand123456789 <157507067+ryand123456789@users.noreply.github.com>
2025-03-30 23:40:14 -0400

    Update platform.js

--- client/platform.js
+++ client/platform.js
@@ -11,14 +11,16 @@ let platformShift = 0;
 let cameraOffsetX = 0;
 let cameraOffsetY = 0;
 let jumpBuffer = false; // Buffer jump input
+let baseHeight = 0;
+let platformColor = 'green'
 
-let xAxis = 0;
-let yAxis = 0;
+// canvas.backgroundColor;
+
 
 // Player object
 let player = {
     x: canvas.width / 2 - 25,
-    y: canvas.height / 2,
+    y: canvas.height / 2 - 25,
     width: 50,
     height: 50,
     speedX: 0,
@@ -48,7 +50,7 @@ let keys = { right: false, left: false, up: false };
 document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight' || e.key === 'd') keys.right = true;
     if (e.key === 'ArrowLeft' || e.key === 'a') keys.left = true;
-    if (e.key === ' ' || e.key === 'ArrowUp' || e.key === 'w') jumpBuffer = true; // Store jump input
+    if (e.key === ' ') jumpBuffer = true; // Store jump input
 });
 
 document.addEventListener('keyup', (e) => {
@@ -99,39 +101,217 @@ function collideWithPlatforms() {
         player.grounded = true;
     }
 }
-/*function gamepadRep(){
-    const gamepads = navigator.getGamepads();
-    const gamepad = gamepads[0];
-    if(gamepad){
-        if(gamepad.buttons[5].pressed){
-            keys.right = true;
-            keys.left = false;
-        }
-        else if(gamepad.buttons[6].pressed)
-        {
-            keys.left = true;
-            keys.right = false;
-        }
-        else{
-            keys.left = false;
-            keys.right = false;            
-        }
 
-        if(gamepad.buttons[4].pressed){
-            keys.up = true;
-        }
-        else{
-            keys.up = false;
-        }
-        if(gamepad.buttons[8].pressed)
-        {
-            location.reload();
-        }
+function colorChange()
+{
+    console.log(baseHeight);
+    if(baseHeight > 4500)
+    {
+        canvas.style.background = "rgb(0, 0, 0)";
+        platformColor = "rgb(255, 255, 255)";
+        console.log("ran");
+        player.color = "rgb(255, 255, 255)";
     }
-}*/
+    else if(baseHeight > 4300)
+        {
+            canvas.style.background = "rgb(44, 44, 44)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 4100)
+        {
+            canvas.style.background = "rgb(255, 0, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 4000)
+        {
+            canvas.style.background = "rgb(255, 0, 76)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3900)
+        {
+            canvas.style.background = "rgb(255, 0, 140)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3800)
+        {
+            canvas.style.background = "rgb(255, 0, 234)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3700)
+        {
+            canvas.style.background = "rgb(225, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3600)
+        {
+            canvas.style.background = "rgb(183, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3500)
+        {
+            canvas.style.background = "rgb(153, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3400)
+        {
+            canvas.style.background = "rgb(119, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3300)
+        {
+            canvas.style.background = "rgb(89, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3200)
+        {
+            canvas.style.background = "rgb(4, 0, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3100)
+        {
+            canvas.style.background = "rgb(0, 47, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 3000)
+        {
+            canvas.style.background = "rgb(0, 89, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2900)
+        {
+            canvas.style.background = "rgb(0, 140, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2800)
+        {
+            canvas.style.background = "rgb(0, 183, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2700)
+        {
+            canvas.style.background = "rgb(0, 225, 255)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2600)
+        {
+            canvas.style.background = "rgb(0, 255, 242)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2500)
+        {
+            canvas.style.background = "rgb(0, 255, 200)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2400)
+        {
+            canvas.style.background = "rgb(0, 255, 128)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2300)
+        {
+            canvas.style.background = "rgb(0, 255, 21)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2200)
+        {
+            canvas.style.background = "rgb(60, 255, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2100)
+        {
+            canvas.style.background = "rgb(200, 255, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 2000)
+        {
+            canvas.style.background = "rgb(255, 251, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1900)
+        {
+            canvas.style.background = "rgb(255, 208, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1800)
+        {
+            canvas.style.background = "rgb(255, 166, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1700)
+        {
+            canvas.style.background = "rgb(255, 123, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1600)
+        {
+            canvas.style.background = "rgb(255, 81, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1500)
+        {
+            canvas.style.background = "rgb(255, 38, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1400)
+        {
+            canvas.style.background = "rgb(197, 30, 0)";
+            platformColor = "rgb(0, 0, 0)";
+            player.color = "rgb(29, 29, 29)";
+        }
+    else if(baseHeight > 1300)
+        {
+            canvas.style.background = "rgb(139, 21, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1200)
+        {
+            canvas.style.background = "rgb(80, 12, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 1000)
+        {
+            canvas.style.background = "rgb(20, 3, 0)";
+            platformColor = "rgb(0, 0, 0)";
+        }
+    else if(baseHeight > 800)
+        {
+            canvas.style.background = "rgb(0, 0, 0)";
+            platformColor = "rgb(3, 24, 16)";
+        }
+    else if(baseHeight > 600)
+        {
+            canvas.style.background = "rgb(0, 13, 73)";
+            platformColor = "rgb(5, 27, 0)";
+        }
+    else if(baseHeight > 400)
+        {
+            canvas.style.background = "rgb(0, 26, 139)";
+            platformColor = "rgb(9, 46, 0)";
+        }
+    else if(baseHeight > 400)
+    {
+        canvas.style.background = "rgb(0, 36, 199)";
+        platformColor = "rgb(13, 63, 0)";
+    }
+    else if(baseHeight > 200)
+    {
+        canvas.style.background = "rgb(44, 128, 255)";
+        platformColor = "rgb(20, 99, 0)";;
+        player.color = "rgb(0, 204, 255)"
+    }
+}
+
+
+
+function resizeCanvas() {
+    // Set canvas width and height to the window's inner width and height
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 // Update function
 function update() {
-    //gamepadRep();
+    resizeCanvas();
+    colorChange();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     if (keys.right) {
@@ -179,18 +359,23 @@ function update() {
     }
     
     // Draw platforms
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = platformColor;
     for (let p of platforms) {
         ctx.fillRect(p.x, p.y, p.width, p.height);
     }
     
     // Draw floor
     ctx.fillRect(floor.x, floor.y, floor.width, floor.height);
-    
+    baseHeight = Math.trunc(cameraOffsetY/8)/10;
     // Draw player
     ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.width, player.height);
-    ctx.fill
+    ctx.fillStyle = 'black';
+    var fontSize = canvas.width*0.05; 
+    ctx.font = `${fontSize}px Arial`;
+
+    ctx.fillText("Height: " + Math.trunc(cameraOffsetY/8)/10 + " meters", canvas.width/20, canvas.height/10, canvas.width*0.4);
+
 
     requestAnimationFrame(update);
 }
